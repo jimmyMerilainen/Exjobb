6d0648a674864bd84d2010fda98b76fe28b671af
Raasmus nilsson <raasmusnilsson@gmail.com>
2022-05-20 15:25:07 +0200

    Registration overlay modal

--- components/ChangeErrorText.js
+++ components/ChangeErrorText.js
@@ -24,11 +24,18 @@ const ChangeErrorText = ({ text, style }) => {
 			'Firebase: Password should be at least 6 characters (auth/weak-password).'
 		) {
 			return 'Lösenordet ska vara minst 6 karaktärer'
+		}
+		if (text === 'Firebase: Error (auth/admin-restricted-operation).') {
+			return 'Något gick helt fel..'
 		} else {
 			return text
 		}
 	}
-	return <Text style={[AppStyles.warning, AppStyles.h3]}>{ChangeText()}</Text>
+	return (
+		<Text style={[AppStyles.warning, AppStyles.h3, { ...style }]}>
+			{ChangeText()}
+		</Text>
+	)
 }
 
 export default ChangeErrorText
