f4fb7b581df2d9fbc931fe066f8471b493a72471
Raasmus nilsson <raasmusnilsson@gmail.com>
2022-05-24 10:27:17 +0200

    Gettings userInfo from Firebase - Rendering played rounds in ProfileScreen in HiistoryFlatlist

--- components/HistoryFlatlist.js
+++ components/HistoryFlatlist.js
@@ -1,21 +1,9 @@
-import React, { useState } from 'react'
+import React from 'react'
 import { View, Text, FlatList } from 'react-native'
-import ButtonDefault from './ButtonDefault'
 import HistoryButton from './HistoryButton'
 import AppStyles from '../styles/AppStyles'
 
-const HistoryFlatlist = () => {
-	const [testarray, setTestarray] = useState([
-		{ course: 'Kode Golf', holes: '18 - hålsbana', info: 'Shop, Restaurang' },
-		{ course: 'S:t Jörgen Golf', holes: '18 - hålsbana', info: 'Range, Shop' },
-		{ course: 'S:t Jörgen Golf', holes: '18 - hålsbana', info: 'Range, Shop' },
-		{ course: 'S:t Jörgen Golf', holes: '18 - hålsbana', info: 'Range, Shop' },
-		{ course: 'S:t Jörgen Golf', holes: '18 - hålsbana', info: 'Range, Shop' },
-		{ course: 'S:t Jörgen Golf', holes: '18 - hålsbana', info: 'Range, Shop' },
-		{ course: 'S:t Jörgen Golf', holes: '18 - hålsbana', info: 'Range, Shop' },
-		{ course: 'S:t Jörgen Golf', holes: '18 - hålsbana', info: 'Range, Shop' },
-	])
-
+const HistoryFlatlist = ({ data }) => {
 	return (
 		<View
 			style={{
@@ -43,13 +31,13 @@ const HistoryFlatlist = () => {
 					</Text>
 				)}
 				contentContainerStyle={{ paddingBottom: 10 }}
-				data={testarray}
+				data={data}
 				keyExtractor={(item, index) => String(index)}
 				renderItem={({ item }) => (
 					<HistoryButton
-						date="23 JUN"
-						course={item.course}
-						holes={item.holes}
+						date={item.date}
+						name={item.name}
+						strokes={item.strokes}
 						style={{ margin: 8 }}
 						onPress={() => {
 							console.log(item)
--- components/RegisterOverlay.js
+++ components/RegisterOverlay.js
@@ -1,16 +1,16 @@
 import React, { useState } from 'react'
 import {
-  Modal,
-  StyleSheet,
-  Text,
-  View,
-  TouchableOpacity,
-  KeyboardAvoidingView,
+	Modal,
+	StyleSheet,
+	Text,
+	View,
+	TouchableOpacity,
+	KeyboardAvoidingView,
 } from 'react-native'
 
 import { createUserWithEmailAndPassword } from 'firebase/auth'
 import { auth, db } from '../firebase'
-import { doc, setDoc } from 'firebase/firestore'
+import { collection, addDoc } from 'firebase/firestore'
 
 import ButtonDefault from './ButtonDefault'
 import AppStyles from '../styles/AppStyles'
@@ -18,125 +18,131 @@ import TextInputDefault from './TextInputDefault'
 import ChangeErrorText from './ChangeErrorText'
 
 const RegisterOverlay = ({ handleOnPress }) => {
-  const [modalVisible, setModalVisible] = useState(false)
-  const [email, setEmail] = useState()
-  const [password, setPassword] = useState()
-  const [username, setUsername] = useState()
-  const [errorMessage, setErrorMessage] = useState()
+	const [modalVisible, setModalVisible] = useState(false)
+	const [email, setEmail] = useState()
+	const [password, setPassword] = useState()
+	const [username, setUsername] = useState()
+	const [errorMessage, setErrorMessage] = useState()
 
-  const handleCreateAccount = () => {
-    createUserWithEmailAndPassword(auth, email, password)
-      .then((userCredential) => {
-        const user = userCredential.user
-        console.log('Account created with email: ', user.email)
+	const handleCreateAccount = () => {
+		createUserWithEmailAndPassword(auth, email, password)
+			.then(() => {
+				try {
+					const docRef = addDoc(collection(db, 'users'), {
+						username: username,
+						email: email,
+						userId: auth.currentUser.uid,
+						playedRounds: [
+							{ date: '23 JUN', name: 'Kode Golf', strokes: 73, tee: 'gul' },
+						],
+					})
+					console.log('Document written with ID: ', docRef)
+				} catch (e) {
+					console.error('Error adding document: ', e)
+				}
+			})
+			.catch((error) => {
+				console.log(error.message)
+				setErrorMessage(error.message)
+			})
+	}
 
-        const newUser = doc(db, 'users', user.uid)
-        const newUserData = { username: username }
-
-        setDoc(newUser, newUserData)
-      })
-      .catch((error) => {
-        console.log(error.message)
-        setErrorMessage(error.message)
-      })
-  }
-
-  return (
-    <View
-      style={[
-        styles.registerView,
-        AppStyles.shadow,
-        AppStyles.grey,
-        AppStyles.border,
-        ,
-        {
-          justifyContent: 'center',
-          alignContent: 'center',
-          alignSelf: 'center',
-        },
-      ]}
-    >
-      <KeyboardAvoidingView behavior={'position'}>
-        <Modal
-          animationType="slide"
-          transparent={true}
-          visible={modalVisible}
-          onRequestClose={() => {
-            setModalVisible(!modalVisible)
-          }}
-        >
-          <View style={[styles.modalView, AppStyles.border, AppStyles.shadow]}>
-            <Text style={[styles.modalText, AppStyles.h1, {}]}>
-              Skapa Konto
-            </Text>
-            <Text style={[styles.modalText, AppStyles.h3]}>
-              Skriv in dina uppgifter nedan
-            </Text>
-            <TextInputDefault
-              textCallback={setUsername}
-              placeholder="Användarnamn"
-            />
-            <TextInputDefault textCallback={setEmail} placeholder="Email" />
-            <TextInputDefault
-              textCallback={setPassword}
-              placeholder="Lösenord"
-            />
-            <ButtonDefault
-              text="Registrera"
-              onPress={() => {
-                handleCreateAccount()
-              }}
-            />
-            <ButtonDefault
-              text="Avbryt"
-              onPress={() => {
-                setModalVisible(!modalVisible)
-              }}
-            />
-            {errorMessage && (
-              <ChangeErrorText
-                text={errorMessage}
-                style={{ marginBottom: 15 }}
-              />
-            )}
-          </View>
-        </Modal>
-      </KeyboardAvoidingView>
-      <TouchableOpacity onPress={() => setModalVisible(!modalVisible)}>
-        <Text style={[styles.textStyle, AppStyles.buttonText]}>Registrera</Text>
-      </TouchableOpacity>
-    </View>
-  )
+	return (
+		<View
+			style={[
+				styles.registerView,
+				AppStyles.shadow,
+				AppStyles.grey,
+				AppStyles.border,
+				,
+				{
+					justifyContent: 'center',
+					alignContent: 'center',
+					alignSelf: 'center',
+				},
+			]}
+		>
+			<KeyboardAvoidingView behavior={'position'}>
+				<Modal
+					animationType="slide"
+					transparent={true}
+					visible={modalVisible}
+					onRequestClose={() => {
+						setModalVisible(!modalVisible)
+					}}
+				>
+					<View style={[styles.modalView, AppStyles.border, AppStyles.shadow]}>
+						<Text style={[styles.modalText, AppStyles.h1, {}]}>
+							Skapa Konto
+						</Text>
+						<Text style={[styles.modalText, AppStyles.h3]}>
+							Skriv in dina uppgifter nedan
+						</Text>
+						<TextInputDefault
+							textCallback={setUsername}
+							placeholder="Användarnamn"
+						/>
+						<TextInputDefault textCallback={setEmail} placeholder="Email" />
+						<TextInputDefault
+							textCallback={setPassword}
+							placeholder="Lösenord"
+						/>
+						<ButtonDefault
+							text="Registrera"
+							onPress={() => {
+								handleCreateAccount()
+							}}
+						/>
+						<ButtonDefault
+							text="Avbryt"
+							onPress={() => {
+								setModalVisible(!modalVisible)
+							}}
+						/>
+						{errorMessage && (
+							<ChangeErrorText
+								text={errorMessage}
+								style={{ marginBottom: 15 }}
+							/>
+						)}
+					</View>
+				</Modal>
+			</KeyboardAvoidingView>
+			<TouchableOpacity onPress={() => setModalVisible(!modalVisible)}>
+				<Text style={[styles.textStyle, AppStyles.buttonText]}>Registrera</Text>
+			</TouchableOpacity>
+		</View>
+	)
 }
 
 const styles = StyleSheet.create({
-  modalView: {
-    height: 550,
-    justifyContent: 'center',
-    alignContent: 'center',
-    alignItems: 'center',
-    marginTop: 22,
-    marginBottom: 100,
-    width: '85%',
-    backgroundColor: 'white',
-    alignSelf: 'center',
-  },
-  registerView: {
-    width: '85%',
-    height: 50,
-    marginBottom: 20,
-    alignItems: 'center',
-    alignSelf: 'center',
-    justifyContent: 'center',
-  },
-  textStyle: {
-    color: 'white',
-    textAlign: 'center',
-  },
-  modalText: {
-    marginBottom: 15,
-    textAlign: 'center',
-  },
+	modalView: {
+		height: 550,
+		justifyContent: 'center',
+		alignContent: 'center',
+		alignItems: 'center',
+		marginTop: 22,
+		marginBottom: 100,
+		width: '85%',
+		backgroundColor: 'white',
+		alignSelf: 'center',
+	},
+	registerView: {
+		width: '85%',
+		height: 50,
+		marginBottom: 20,
+		alignItems: 'center',
+		alignSelf: 'center',
+		justifyContent: 'center',
+	},
+	textStyle: {
+		color: 'white',
+		textAlign: 'center',
+	},
+	modalText: {
+		marginBottom: 15,
+		textAlign: 'center',
+	},
 })
 
 export default RegisterOverlay
