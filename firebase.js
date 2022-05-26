c25dc0b8c25fc5bf113654c02520b45a741e26ee
Raasmus nilsson <raasmusnilsson@gmail.com>
2022-05-26 21:11:08 +0200

    AsyncStorage Warning fixed

--- firebase.js
+++ firebase.js
@@ -1,19 +1,22 @@
 import { initializeApp } from 'firebase/app'
-import { getAuth } from 'firebase/auth'
+import { getReactNativePersistence, initializeAuth } from 'firebase/auth'
 import { getFirestore } from 'firebase/firestore'
+import AsyncStorage from '@react-native-async-storage/async-storage'
 
 const firebaseConfig = {
-  apiKey: 'AIzaSyA5DtEslkH9Ib12GjwGMgTKyGrULcMlNHA',
-  authDomain: 'golfhacker-634d5.firebaseapp.com',
-  projectId: 'golfhacker-634d5',
-  storageBucket: 'golfhacker-634d5.appspot.com',
-  messagingSenderId: '389097613656',
-  appId: '1:389097613656:web:22dee79864da36caa8dd2d',
+	apiKey: 'AIzaSyA5DtEslkH9Ib12GjwGMgTKyGrULcMlNHA',
+	authDomain: 'golfhacker-634d5.firebaseapp.com',
+	projectId: 'golfhacker-634d5',
+	storageBucket: 'golfhacker-634d5.appspot.com',
+	messagingSenderId: '389097613656',
+	appId: '1:389097613656:web:22dee79864da36caa8dd2d',
 }
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig)
-const auth = getAuth(app)
 const db = getFirestore(app)
+const auth = initializeAuth(app, {
+	persistence: getReactNativePersistence(AsyncStorage),
+})
 
 export { app, auth, db }
